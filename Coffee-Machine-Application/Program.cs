71886dbfcd0a6ae2d7e9fbdd67a4bd3f8e65339b
shubhashree.sathish <shubhashree.sathish@solitontech.com>
2026-09-22 11:37:48 +0530

    docs: Add .gitignore

---
+++ Coffee-Machine-Application/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Coffee_Machine_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
